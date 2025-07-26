3a707215609cbb0a8efd3ef38632882dc4d48260
Muskan Gupta <muskan_gupt@yahoo.com>
2025-07-26 09:57:52 -0700

    Post router fixed

--- routers/postRouters.ts
+++ routers/postRouters.ts
@@ -3,6 +3,7 @@ import express from "express";
 import * as database from "../controller/postController";
 const router = express.Router();
 import { ensureAuthenticated } from "../middleware/checkAuth";
+import { addPost,getPost,addComment,editPost,deletePost } from '../fake-db'
 
 router.get("/", async (req, res) => {
   const posts = await database.getPosts(20);
@@ -11,39 +12,60 @@ router.get("/", async (req, res) => {
 });
 
 router.get("/create", ensureAuthenticated, (req, res) => {
-  res.render("createPosts");
+  res.render("createPosts",{post:null});
 });
 
 router.post("/create", ensureAuthenticated, async (req, res) => {
-  // ⭐ TODO
+  // ⭐ DONE TODO
+  const postContent = req.body;
+  await addPost(postContent.title,postContent.link,req.user.id,postContent.description,postContent.subgroup);
+  res.redirect('/posts')
 });
 
-router.get("/show/:postid", async (req, res) => {
-  // ⭐ TODO
-  res.render("individualPost");
+router.get("/show/:postid",ensureAuthenticated, async (req, res) => {
+  // ⭐ DONE TODO
+    const user = await req.user;
+    const post = await getPost(req.params.postid);
+    res.render("individualPost",{post,user});
 });
 
 router.get("/edit/:postid", ensureAuthenticated, async (req, res) => {
-  // ⭐ TODO
+  // ⭐DONE TODO
+  const user = await req.user;
+  const post = await getPost(req.params.postid);
+  res.render("createPosts",{post});
 });
 
 router.post("/edit/:postid", ensureAuthenticated, async (req, res) => {
-  // ⭐ TODO
+  // ⭐DONE TODO
+  const editPostContent = req.body;
+  await editPost(req.params.postid,editPostContent);
+
+  const user = await req.user;
+  const post = await getPost(req.params.postid);
+  res.render("individualPost",{post,user});
 });
 
 router.get("/deleteconfirm/:postid", ensureAuthenticated, async (req, res) => {
-  // ⭐ TODO
+  // ⭐DONE TODO
+  const user = await req.user;
+  const post = await getPost(req.params.postid);
+  res.render("deleteConfirm",{post,user});
 });
 
 router.post("/delete/:postid", ensureAuthenticated, async (req, res) => {
   // ⭐ TODO
+  await deletePost(req.params.postid);
+  res.redirect('/posts')
 });
 
-router.post(
-  "/comment-create/:postid",
-  ensureAuthenticated,
-  async (req, res) => {
-    // ⭐ TODO
+router.post("/comment-create/:postid", ensureAuthenticated, async (req, res) => {
+    // ⭐ DONE TODO
+    const commentContent = req.body;
+    const user = await req.user;
+    await addComment(commentContent.postId,user.id,commentContent.description);
+    const post = await getPost(commentContent.postId);
+    res.render("individualPost",{post,user});
   }
 );
 
