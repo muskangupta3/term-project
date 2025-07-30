edce61c192133097ffce0fbe06f03e82714884c4
Muskan Gupta <muskan_gupt@yahoo.com>
2025-07-30 01:21:45 -0700

    Integrated prisma sqlite db in sub router

--- routers/subsRouters.ts
+++ routers/subsRouters.ts
@@ -3,17 +3,29 @@ import express from "express";
 import * as database from "../controller/postController";
 const router = express.Router();
 import { getSubs,getPosts } from '../fake-db'
+import { PrismaClient } from "@prisma/client";
+
+const db = new PrismaClient();
 
 router.get("/list",ensureAuthenticated, async (req, res) => {
   // ⭐DONE TODO
-  const subs = await getSubs();
+  //const subs = await getSubs();
+  const subs = await db.subgroup.findMany();
   res.render("subs",{subs});
 });
 
 router.get("/show/:subname",ensureAuthenticated, async (req, res) => {
   // ⭐DONE TODO
   const subName = req.params.subname;
-  const posts = await getPosts(5,subName);
+  //const posts = await getPosts(5,subName);
+  const posts = await db.post.findMany({
+    take:5,
+    where:{
+    sub: {
+      name: subName
+    }
+  }
+  })
   const user = req.user;
   res.render("sub",{posts,user,subName});
 });
